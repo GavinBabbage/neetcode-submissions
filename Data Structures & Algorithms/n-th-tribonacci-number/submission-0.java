2d7bb5e13ade6e88e2748b25374bdca6380ae358
GavinBabbage <72704931+GavinBabbage@users.noreply.github.com>
2026-06-01 23:13:50 +0200

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int tribonacci(int n) {
+        int x = 0;
+        int y = 1;
+        int z = 1;
+        int w = x + y + z;
+
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 1;
+        }
+        int index = 3;
+        while(index<n){
+            x = y;
+            y = z;
+            z = w;
+            w = x + y + z;
+            index++;
+        }
+        System.out.println(w);
+        return w;
+    }
+}
